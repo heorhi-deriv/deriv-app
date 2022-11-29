2f3a411f4f025d89051069a41df989c4aed75632
George Usynin <103181646+george-usynin-binary@users.noreply.github.com>
2022-11-29 11:26:48 +0300

    george / refactor selfie step, country selection,  improve useReducer (#21)
    * refactor: add custom reducer
    
    * fix: wrap actions with useCallback
    
    * refactor: country step
    
    * refactor: selfie step
    
    * refactor: remove classNme from country-selector
    
    * perf: add react memo
    
    * fix: selfie value

--- packages/cashier/src/pages/payment-agent/payment-agent-signup/signup-wizard/steps-reducer.ts
+++ packages/cashier/src/pages/payment-agent/payment-agent-signup/signup-wizard/steps-reducer.ts
@@ -1,45 +1,59 @@
+import { useCallback, useReducer } from 'react';
+import { ResidenceList } from '@deriv/api-types';
 import { TSelfie } from '../signup-wizard-steps/selfie-step/selfie-step';
 
 type TStepsState = {
     selfie: {
         selfie_with_id: TSelfie;
     } | null;
-    is_selfie_step_enabled: boolean;
+    selected_country?: ResidenceList[number];
 };
 
 const ACTION_TYPES = {
     SET_SELFIE: 'SET_SELFIE',
-    SET_SELFIE_STEP_ENABLED: 'SET_SELFIE_STEP_ENABLED',
+    SET_SELECTED_COUNTRY: 'SET_SELECTED_COUNTRY',
 } as const;
 
 // Action creators
-export const setSelfie = (value: { selfie_with_id: TSelfie }) => {
+const setSelfieAC = (value: { selfie_with_id: TSelfie }) => {
     return {
         type: ACTION_TYPES.SET_SELFIE,
         value,
     };
 };
 
-export const setSelfieStepEnabled = (value: boolean) => {
+const setSelectedCountryAC = (value?: ResidenceList[number]) => {
     return {
-        type: ACTION_TYPES.SET_SELFIE_STEP_ENABLED,
+        type: ACTION_TYPES.SET_SELECTED_COUNTRY,
         value,
     };
 };
 
 // Initial state
-export const initial_state = { selfie: null, is_selfie_step_enabled: false };
+const initial_state = { selected_country: {}, selfie: null, is_selfie_step_enabled: false };
 
 // Reducer
-export const stepReducer = (state: TStepsState, action: TActionsTypes): TStepsState => {
+const stepReducer = (state: TStepsState, action: TActionsTypes): TStepsState => {
     switch (action.type) {
         case ACTION_TYPES.SET_SELFIE:
             return { ...state, selfie: action.value };
-        case ACTION_TYPES.SET_SELFIE_STEP_ENABLED:
-            return { ...state, is_selfie_step_enabled: action.value };
+        case ACTION_TYPES.SET_SELECTED_COUNTRY:
+            return { ...state, selected_country: action.value };
         default:
             return state;
     }
 };
 
-export type TActionsTypes = ReturnType<typeof setSelfie | typeof setSelfieStepEnabled>;
+export const usePaymentAgentSignupReducer = () => {
+    const [steps_state, dispatch] = useReducer(stepReducer, initial_state);
+
+    const setSelfie = useCallback((value: { selfie_with_id: TSelfie }) => dispatch(setSelfieAC(value)), []);
+    const setSelectedCountry = useCallback(
+        (value?: ResidenceList[number]) => dispatch(setSelectedCountryAC(value)),
+        []
+    );
+
+    return { steps_state, setSelectedCountry, setSelfie };
+};
+
+type TActionsTypes = ReturnType<typeof setSelfieAC | typeof setSelectedCountryAC>;
