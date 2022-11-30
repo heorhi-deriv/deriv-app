0cc6ffcf48f670b7345785a8e64a994cf84a28ed
Hamid Yaftian <hamid@re-work.dev>
2022-11-30 17:58:17 +0330

    chore: enhance structure

--- packages/cashier/src/pages/payment-agent/payment-agent-signup/components/application-status-dialog/provider.tsx
+++ packages/cashier/src/pages/payment-agent/payment-agent-signup/components/application-status-dialog/provider.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Icon } from '@deriv/components';
-import { Localize } from '@deriv/translations';
 
 type TProvider = {
     [k: string]: {
         icon: JSX.Element;
         title: string;
-        text: JSX.Element;
-        hint: JSX.Element | null;
+        text: string;
+        hint: string | null;
         button: string | null;
     };
 };
@@ -16,47 +15,35 @@ const provider: TProvider = {
     success: {
         icon: <Icon icon='IcCheckmarkCircle' custom_color='var(--status-success)' size={72} />,
         title: "We've received your application.",
-        text: (
-            <Localize i18n_default_text='We will notify you via email about the status of your application within 3 working days.' />
-        ),
-        hint: (
-            <Localize i18n_default_text='Note: If your account balance falls below 3,000.00 USD, you’ll need to resubmit your application.' />
-        ),
+        text: 'We will notify you via email about the status of your application within 3 working days.',
+        hint: 'Note: If your account balance falls below 3,000.00 USD, you’ll need to resubmit your application.',
         button: null,
     },
     pending_before_poi: {
         icon: <Icon icon='IcDocsSubmit' size={72} />,
         title: 'Documents pending for review',
-        text: (
-            <Localize i18n_default_text='We are still processing your details. In the meantime, please continue to provide your Proof of Address, selfie, and PA information.' />
-        ),
+        text: 'We are still processing your details. In the meantime, please continue to provide your Proof of Address, selfie, and PA information.',
         hint: null,
         button: 'Continue',
     },
     pending_after_poi: {
         icon: <Icon icon='IcDocsSubmit' size={72} />,
         title: 'Documents pending for review',
-        text: (
-            <Localize i18n_default_text='We are still processing your details. You will receive an email about your application status within 3 working days. ' />
-        ),
+        text: 'We are still processing your details. You will receive an email about your application status within 3 working days. ',
         hint: null,
         button: null,
     },
     failed_first_attempt: {
         icon: <Icon icon='IcPoiError' size={72} />,
         title: 'ID verification failed',
-        text: (
-            <Localize i18n_default_text='We were unable to verify your ID due to external technical issues. Please try again.' />
-        ),
+        text: 'We were unable to verify your ID due to external technical issues. Please try again.',
         hint: null,
         button: 'Try again',
     },
     failed_second_attempt: {
         icon: <Icon icon='IcPoiError' size={72} />,
         title: 'ID verification failed',
-        text: (
-            <Localize i18n_default_text='We were unable to verify your ID due to external technical issues. Please continue to try another verification process.' />
-        ),
+        text: 'We were unable to verify your ID due to external technical issues. Please continue to try another verification process.',
         hint: null,
         button: 'Continue',
     },
